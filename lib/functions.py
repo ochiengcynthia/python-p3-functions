272929db239f788c2b01af5c391b9633484c3bc8
ochiengcynthia <126596182+ochiengcynthia@users.noreply.github.com>
2023-05-16 08:51:53 +0300

    completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
-
+def my_function(param):
+    print("Running my_function")
+    return print(param + 1)
+    
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
+    print(f"Hello, {name}!")
     pass
 
+
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
 def add(num1, num2):
+    return num1 + num2
+    print(add)
     pass
 
 def halve(number):
+    return number / 2
     pass
+
+my_function(1)
